9bd24f9344e150867f7223e2ddbbd5f510f73868
Rodrigo Yoshioka <rodrigo.yoshioka@grupozap.com>
2021-08-25 12:12:17 -0300

    feat: create api test

---
+++ pages/api/test-api.js
@@ -0,0 +1,9 @@
+
+export default function handler(req, res) {
+  res.status(200).json({
+    name: 'Rodrigo Yoshioka',
+    email: 'rodrigo.yoshioka@gmail.com', 
+    telefone: '(011)9999-9999',
+    listingId: '123213123123'
+  })
+}
