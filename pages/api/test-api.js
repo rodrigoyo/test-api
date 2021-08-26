a435f8c3509259ec67ae8bc05d4940cc065bdac9
Rodrigo Yoshioka <rodrigo.yoshioka@grupozap.com>
2021-08-26 16:28:44 -0300

    fix: more fields

--- pages/api/test-api.js
+++ pages/api/test-api.js
@@ -1,9 +1,19 @@
 
 export default function handler(req, res) {
   res.status(200).json({
-    name: 'Rodrigo Yoshioka',
-    email: 'rodrigo.yoshioka@gmail.com', 
-    telefone: '(011)9999-9999',
-    listingId: '123213123123'
+    'id': '123123',
+    'createdTime':  '2020-20-20 00:00:00',
+    'form': {
+      'name': 'Nome do formulário'
+    },
+    'fieldData': {
+      'name': 'Rodrigo Yoshioka',
+      'phone': '(11) 9999-9999',
+      'email': 'rodrigo.yoshioka@gmail.com'
+    },
+    'adset_id': '3333',
+    'adset_name': 'Adset Name',
+    'ad_name': 'Ad name',
+    'retailer_item_id': '1232131',
   })
 }
